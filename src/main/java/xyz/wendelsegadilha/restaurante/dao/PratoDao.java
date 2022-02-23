8acc41ff6f59d9f748f7fe6d964b875273487d8f
Wendel Segadilha <wendelsegadilha99@gmail.com>
2022-02-22 21:15:43 -0300

    implementado CRUD na classe PratoDao

--- src/main/java/xyz/wendelsegadilha/restaurante/dao/PratoDao.java
+++ src/main/java/xyz/wendelsegadilha/restaurante/dao/PratoDao.java
@@ -16,4 +16,17 @@ public class PratoDao {
         this.entityManager.persist(prato);
         System.out.println("Entidade cadastrada: " + prato.toString());
     }
+
+    public Prato consultar(Integer id){
+        return this.entityManager.find(Prato.class, id);
+    }
+
+    public void atualizar(Prato prato){
+        this.entityManager.merge(prato);
+    }
+
+    public void excluir(Prato prato){
+        this.entityManager.remove(prato);
+    }
+
 }
